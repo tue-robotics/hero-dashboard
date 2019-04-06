e3295479b7174adecb891236936de19ef1c6ba43
SamAlexandrov <alexandrov.samuil@gmail.com>
2019-04-06 14:30:28 +0200

    Add simple progressbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ProgressBar from 'react-bootstrap/ProgressBar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get asdf, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Dashboard
+        <ProgressBar now={20} />
       </div>
     );
   }
