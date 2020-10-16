2a8140fed2236252d1db5bcaca0b4de69347bebd
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-10-16 12:00:35 +0200

    Wait for ROS to close to stop logging after tests are done

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,33 @@ import Indicator from './Indicator';
 import Battery from './Battery';
 import AutoRos from './ros';
 
-test('App', async () => {
+/**
+ * timeout [ms] needs to be smaller than RECONNECT_TIMEOUT
+ * sleeptime [ms] needs to be smaller than timeout to have multiple checks
+ */
+function waitForRosClosed(timeout, sleeptime) {
+  return new Promise(resolve => {
+    var start_time = Date.now();
+    function checkRosStatus() {
+      if (AutoRos.status === 'closed') {
+        resolve();
+      } else if (Date.now() > start_time + timeout) {
+        console.log('ROS not closed yet closed before timeout');
+        resolve();
+      } else {
+        window.setTimeout(checkRosStatus, sleeptime);
+      }
+    }
+    checkRosStatus();
+  });
+}
+
+afterAll(async () => {
+  AutoRos.ros.close();
+  await waitForRosClosed(4500, 100);
+})
+
+test('App', () => {
   const { container } = render(<App />);
   const IndicatorElement = container.querySelector('div.Indicator');
   expect(IndicatorElement).not.toBeNull();
@@ -13,13 +39,13 @@ test('App', async () => {
   expect(BatteryElement).toBeNull();
 });
 
-test('Indicator', async () => {
+test('Indicator', () => {
   const { container } = render(<Indicator ros={AutoRos.ros} />);
   const IndicatorElement = container.querySelector('div.Indicator');
   expect(IndicatorElement).not.toBeNull();
 });
 
-test('Empty Battery', async () => {
+test('Empty Battery', () => {
   const { container } = render(<Battery topic='battery' ros={AutoRos.ros} />);
   const BatteryElement = container.querySelector('div.Battery');
   expect(BatteryElement).toBeNull();
