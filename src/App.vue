996f25623704b0e3a7cd8549e922cdfa4f31993e
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2026-01-29 14:25:03 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,77 @@
 <template>
   <div id="app">
-    <b-row>
-      <b-col>
-        <Indicator
-          id="indicator"
-          :ros="autoRos.ros"
-        />
-      </b-col>
-      <b-col>
-        <Battery
-          id="battery"
-          :ros="autoRos.ros"
-        />
-      </b-col>
-    </b-row>
+    <div class="row">
+      <div class="col">
+        <Indicator id="indicator" :ros="ros" />
+      </div>
+      <div class="col">
+        <Battery id="battery" :ros="ros" />
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-/* eslint new-cap: ["error", { "properties": false }] */
+<script setup lang="ts">
+  import { markRaw, onMounted } from "vue";
+  import { AutoRos } from "auto-ros";
+  import { Battery, Indicator } from "hero-vue";
 
-import AutoRos from 'auto-ros'
-import { Battery, Indicator } from 'hero-vue'
+  const autoRos = new AutoRos();
+  const ros = markRaw(autoRos.ros);
+  const endPoint = "ws://localhost:9090";
 
-import { BCol, BRow } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-export default {
-  name: 'HeroDashboard',
-  components: {
-    Battery,
-    Indicator,
-    'b-col': BCol,
-    'b-row': BRow
-  },
-  data () {
-    return {
-      autoRos: new AutoRos(),
-      endPoint: 'ws://localhost:9090'
+  onMounted(async () => {
+    const host = (await window?.args?.host()) || null;
+    let url = endPoint;
+    if (host) {
+      url = `ws://${host}:9090`;
     }
-  },
-  mounted () {
-    const argv = require('@electron/remote').process.argv
-    const index = argv.length - 1
-    let url = this.endPoint
-    if (index > 0) {
-      const host = argv[index]
-      url = `ws://${host}:9090`
-    }
-    this.autoRos.connect(url)
-  }
-}
+
+    autoRos.connect(url);
+  });
 </script>
 
 <style>
-body {
-  margin: 0;
-  padding: 0;
-  width: 375px;
-  height: 60px;
-  line-height: 1.15;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-#indicator {
-  margin-left: 30px;
-  margin-right: 30px;
-  margin-top: 10px;
-  margin-bottom: 10px;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    width: 375px;
+    height: 60px;
+    line-height: 1.15;
+    font-family:
+      -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-#battery {
-  position: fixed;
-  top: 5px;
-  left: 120px;
-}
-#battery_col {
-  width: 120px;
-  padding-left: 10px;
-  padding-right: 10px;
-  margin-left: 0px;
-  margin-right: 0px;
-}
-.row {
-  display: inline-flex;
-}
-#battery h5 {
-  margin: 0px;
-  font-size: 1.0rem;
-  font: inherit;
-}
-#batteryProgress {
-  height: 1.5rem;
-}
+  #indicator {
+    margin-left: 30px;
+    margin-right: 30px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  #battery {
+    position: fixed;
+    top: 5px;
+    left: 120px;
+  }
+  #battery .col {
+    width: 120px;
+    padding-left: 10px;
+    padding-right: 10px;
+    margin-left: 0px;
+    margin-right: 0px;
+  }
+  #battery .h5 {
+    margin: 0px;
+    font-size: 1rem;
+    font: inherit;
+  }
+  #battery .progress {
+    height: 1.5rem;
+  }
+  .row {
+    display: inline-flex;
+  }
 </style>
